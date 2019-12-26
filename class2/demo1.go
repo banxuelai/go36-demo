c1f7017750b423a3a4fcd0f106f23925de939fb3
belly <belly@mail.com>
2019-12-26 14:12:07 +0800

    demo1

---
+++ class2/demo1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var name string
+
+func init() {
+	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+}
+
+func main() {
+	fmt.Printf("Hello, %s !\n", name)
+}
